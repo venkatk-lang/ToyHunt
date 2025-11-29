8dc0a4cc12cb01383996ba54545c63c5a13d322f
venkatk-lang <242193736+venkatk-lang@users.noreply.github.com>
2025-11-29 17:53:49 +0530

    2d setup with iacg setup

--- Assets/Game/GameManager.cs
+++ Assets/Game/GameManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using IACGGames;
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     [Header("References")]
     public RoundGenerator roundGenerator;
